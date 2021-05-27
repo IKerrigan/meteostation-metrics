ea6d84081a65535ab64ba3a2d3146947cfb474f1
Iker <63158365+IKerrigan@users.noreply.github.com>
2021-05-28 00:35:28 +0300

    fix(): rework design

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-  let history = await req.metricStorage.getLast10();
+  const history = await req.metricStorage.getLast10();
 
   res.render('index', {
-    title: 'Meteostation',
     actual: history[0] || {},
+    title: 'Meteostation',
     history,
   });
 });
